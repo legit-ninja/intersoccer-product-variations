ffc9c3b6b8b306f95ef3140500835012b1c79ce5
Jeremy Lee <jlee@samaritan.org>
2025-10-06 20:51:37 -0400

    bump version to new format

--- intersoccer-product-variations.php
+++ intersoccer-product-variations.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: InterSoccer Product Variations
  * Description: Custom plugin for InterSoccer Switzerland to manage events and bookings.
- * Version: 1.4.61
+ * Version: 1.10.6
  * Author: Jeremy Lee
  * Text Domain: intersoccer-product-variations
  * Domain Path: /languages
